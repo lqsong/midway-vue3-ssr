e6a55ac5993365685a8a9612a64b51404e2521d3
lqs <957698457@qq.com>
2023-11-15 09:41:50 +0800

    reset component Pagination css

--- web/components/Pagination/base.vue
+++ web/components/Pagination/base.vue
@@ -66,13 +66,16 @@ const getPageUrl = (val: number)=> {
                     <i class="el-icon"><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M340.864 149.312a30.592 30.592 0 0 0 0 42.752L652.736 512 340.864 831.872a30.592 30.592 0 0 0 0 42.752 29.12 29.12 0 0 0 41.728 0L714.24 534.336a32 32 0 0 0 0-44.672L382.592 149.376a29.12 29.12 0 0 0-41.728 0z"></path></svg></i>
                 </router-link>
               </li>
-         </ul>        
+         </ul>
     </div>
 </template>
 <style lang="scss" scoped>
 .el-pager {
     li {
         padding: 0;
+        * {
+          pointer-events:inherit;
+        }
     }
     .link {
         color: var(--el-text-color-regular);
